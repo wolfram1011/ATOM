93d9b042afc9b8ab906b50b2c61df33d47f6ccc2
wolfram1011 <wolframfacturas@gmail.com>
2023-03-22 22:50:36 -0600

    'test_error'

---
+++ angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,13 @@
+// import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+
+}
